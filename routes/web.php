7c347f2acd7dce8179e5073586e17d3b591cb4a6
simoneluca00 <si.zaino@libero.it>
2022-05-30 19:43:23 +0200

    Index and show added

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'PageController@index') -> name('home');
+Route::get('/', 'PageController@index') -> name('index');
+
+Route::get('/{id}', 'PageController@show') -> name('show');
